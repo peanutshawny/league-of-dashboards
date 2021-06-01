55ab894aa968e18df699336f25555c62512f4433
Shawn Liu <shawnliu30@gmail.com>
2021-05-31 23:27:34 -0400

    deleted old queries folder

--- queries/champ_winrate.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-game_instance.Win,
-champion.Name
-FROM game_instance
-INNER JOIN game_champ ON game_instance.GameID = game_champ.GameID
-INNER JOIN champion ON game_champ.ChampionID = champion.ID
-WHERE champion.name = "Ashe"
