0e937d452b8f6b7bd648cb71d889860b1abf38e5
Shawn Liu <shawnliu30@gmail.com>
2021-06-01 23:34:59 -0400

    added input box and output container for winrate

--- app/dash_app.py
+++ app/dash_app.py
@@ -6,16 +6,42 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
+from dash_data import winrate_df, pickrate_df, damage_df
+
 app = dash.Dash(__name__)
 
+# app layout
 app.layout = html.Div([
 
     html.H1('League of Legends Dashboard', style={'text-align': 'center'}),
 
-    html.Div(id='output_container', children=[]),
-    html.Br()
+    dcc.Input(
+        id='input_champ',
+        type='text',
+        placeholder='Input Champion'
+    ),
+
+    html.Div(id='winrate_container', children=[]),
+    html.Br(),
+
+    dcc.Graph(id='winrate_pie_chart', figure={})
 
 ])
+
+
+# callbacks to connect plotly graphs with dash components
+@app.callback(
+    Output(component_id='winrate_container', component_property='children'),
+    Input(component_id='input_champ', component_property='value')
+)
+def winrate_pie_chart(champ_slctd):
+    container = f'The champion chosen by user was: {champ_slctd}'
+
+    winrate_dff = winrate_df.copy()
+    winrate_dff = winrate_dff[winrate_dff['champion'] == champ_slctd]
+
+    return container
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
